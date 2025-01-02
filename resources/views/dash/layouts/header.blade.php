8e160177fc5a5579fd76d4a098f273fe2c7c51d2
Admin <admin@Admins-MacBook-Pro.local>
2025-01-02 17:37:26 +0400

    reg add

--- resources/views/dash/layouts/header.blade.php
+++ resources/views/dash/layouts/header.blade.php
@@ -38,6 +38,6 @@
 
   <!-- Preloader -->
   <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="{{ asset('fav.svg') }}" alt="AdminLTELogo" height="60" width="60">
+    <img class="animation__shake" src="{{ asset('fav.svg') }}" alt="tap" height="60" width="60">
   </div>
 
