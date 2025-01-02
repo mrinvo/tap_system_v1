6f7b41f0d5145e797f125ef692a36048d3ff4d31
Admin <admin@Admins-MacBook-Pro.local>
2025-01-02 17:15:30 +0400

    fav

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,8 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="icon" href="{{ asset('fav.svg') }}" type="image/x-icon">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <title>{{ config('app.name', 'Tap Erp') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
