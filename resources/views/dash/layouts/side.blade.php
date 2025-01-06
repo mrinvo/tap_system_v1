06f94dee47d25ae2f4933d2a8cf11a6b834a1dba
Admin <admin@Admins-MacBook-Pro.local>
2025-01-06 14:30:26 +0400

    home routes

--- resources/views/dash/layouts/side.blade.php
+++ resources/views/dash/layouts/side.blade.php
@@ -1,7 +1,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="{{ route('dashboard') }}" class="brand-link">
       <img src="/fav.svg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Tap ERP</span>
     </a>
@@ -14,7 +14,7 @@
           <img src="/dash/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">{{ @Auth::user()->name; }}</a>
+          <a href="{{ route('profile.edit') }}" class="d-block">{{ @Auth::user()->name; }}</a>
         </div>
       </div>
 
